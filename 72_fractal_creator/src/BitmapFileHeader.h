7bb7756a60d9f4dcbff5bdbd196d761ca2c936d3
Eloi Lemaire <lemaire.eloi@gmail.com>
2024-03-05 16:08:13 +0100

    Adds Tutorial 73 – Bitmap File Headers

---
+++ 72_fractal_creator/src/BitmapFileHeader.h
@@ -0,0 +1,26 @@
+/*
+ * BitmapFileHeader.h
+ *
+ *  Created on: 4 mars 2024
+ *      Author: eloilemaire
+ */
+
+#ifndef BITMAPFILEHEADER_H_
+#define BITMAPFILEHEADER_H_
+
+#include <cstdint>
+
+#pragma pack(2)
+
+struct BitmapFileHeader{
+	char header[2]{'B', 'M'};
+	int32_t fileSize;
+	int32_t reserved{0};
+	int32_t dataOffset;
+
+};
+
+
+
+
+#endif /* BITMAPFILEHEADER_H_ */
