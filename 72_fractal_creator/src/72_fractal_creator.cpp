462de3768499eb702921fa5c4113b4b5c14267d2
Eloi Lemaire <lemaire.eloi@gmail.com>
2024-03-05 16:07:47 +0100

    Adds Tutorial 72 – Fractal Creator – Hello World

---
+++ 72_fractal_creator/src/72_fractal_creator.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : 72_fractal_creator.cpp
+// Author      : Eloi Lemaire
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "BitmapFileHeader.h"
+#include "BitmapInfoHeader.h"
+using namespace std;
+
+
+int main() {
+	cout << "Hello world!" << endl; // prints Hello world!
+	return 0;
+}
